02b45fb5db25bfb9351ab0633d3796f6437b23da
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-15 18:08:18 +0530

    find frequency in sentence

---
+++ UC-1_Find_Frequency_in_Sentence/MyMapNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_1_Find_Frequency_in_Sentence
+{
+    public class MyMapNode<K, V>
+    {
+        public int size;
+        public  LinkedList<KeyValue<K,V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if(linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public int GetArrayPosition(K Key)
+        {
+            int position = Key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K Key)
+        {
+            int position = GetArrayPosition(Key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(Key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value};
+            linkedList.AddLast(item);
+        }
+        public struct KeyValue<k ,v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
---
+++ UC-1_Find_Frequency_in_Sentence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC_1_Find_Frequency_in_Sentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash0 = hash.Get("0");
+            string hash1 = hash.Get("1");
+            string hash2 = hash.Get("2");
+            string hash3 = hash.Get("3");
+            string hash4 = hash.Get("4");
+            string hash5 = hash.Get("5");
+
+            Console.WriteLine("Sequence of sentence are: ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("\"" + hash0 + " " + hash1 + " " + hash2 + " " + hash3 + " " + hash4 + " " + hash5 + "\"");
+
+        }
+    }
+}
